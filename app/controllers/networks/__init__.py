51411d4bc70d66e53ea61512c2d2b4341d74a300
Fábio Ewerton <fabioew89@gmail.com>
2025-01-10 09:43:10 -0400

    fix: sort imports in networks/__init__.py

--- app/controllers/networks/__init__.py
+++ app/controllers/networks/__init__.py
@@ -1,9 +1,12 @@
-from .get_interface_configuration import get_interface_configuration
-from .get_interface_summary import get_interface_summary
+from .set_static_route import set_static_route
 from .set_interface_unit import set_interface_unit
+from .get_interface_summary import get_interface_summary
+from .get_interface_configuration import get_interface_configuration
+
 
 __all__ = [
+    'set_static_route',
+    'set_interface_unit',
     'get_interface_summary',
     'get_interface_configuration',
-    'set_interface_unit',
 ]
