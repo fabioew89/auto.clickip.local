6f3b1e275e7a89bb8a2bd13e90ae17b7be504ee1
Fábio Ewerton <fabioew89@gmail.com>
2025-01-10 09:43:41 -0400

    feat: app new netmiko file set_static_route.py

---
+++ app/controllers/networks/set_static_route.py
@@ -0,0 +1,15 @@
+from netmiko import ConnectHandler
+
+
+# Cria um rota estatica em um dispositivo Juniper
+def set_router_static(hostname, username, password, network_dest, next_hop):
+    router = {
+        'device_type': 'juniper',
+        'host': hostname,
+        'username': username,
+        'password': password,
+    }
+    ssh = ConnectHandler(**router)
+    output = ssh.send_command(f'set routing-options static route {network_dest} next-hop {next_hop}')  # noqa: E501
+    ssh.disconnect()
+    return output
