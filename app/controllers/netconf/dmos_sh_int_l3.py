8621b6045413c56998acd51ab308ce3713dc656e
Fábio Ewerton <fabioew89@gmail.com>
2025-01-30 13:43:37 -0400

    feat: add some scripts with netconf

---
+++ app/controllers/netconf/dmos_sh_int_l3.py
@@ -0,0 +1,34 @@
+from rich import print
+from ncclient import manager
+from xml.dom.minidom import parseString
+
+
+router = {
+    'host': '100.127.0.254',
+    'port': 830,
+    'username': 'netconf',
+    'password': 'tngKadTvL2W4Er',
+    'hostkey_verify': False,
+}
+
+
+def connect_to_router():
+    filter = '''
+    <filter xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
+        <config xmlns="urn:dmos">
+            <interface>
+                <l3 xmlns="urn:dmos:dmos-ip-application"/>
+            </interface>
+        </config>
+    </filter>
+    '''
+    try:
+        with manager.connect(**router) as m:
+            output = m.get_config('running', filter=filter).data_xml
+            xml_output = parseString(output).toprettyxml(indent=(' ' * 2))
+            print(xml_output)
+    except Exception as e:
+        print(f'Erro ao conectar: {e}')
+
+
+connect_to_router()
