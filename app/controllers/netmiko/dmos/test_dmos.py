f3df11afeae8a7121bdfd90362c378b4fc518d60
Fábio Ewerton <fabioew89@gmail.com>
2025-02-04 09:33:16 -0400

    netmiko dmos

---
+++ app/controllers/netmiko/dmos/test_dmos.py
@@ -0,0 +1,40 @@
+from netmiko import ConnectHandler
+from rich import print
+
+
+def test_dmos(hostname, username, password):
+
+    device = {
+        'device_type': 'cisco_ios',
+        'host': hostname,
+        'username': username,
+        'password': password,
+        'port': 22,
+        'verbose': True,
+        'global_delay_factor': 2,
+        'read_timeout_override': 5,
+    }
+
+    try:
+        ssh = ConnectHandler(**device)
+        print(f'[green]Conectado ao {hostname}✅[/green]')
+
+        output = ssh.send_command("show ip interface brief")
+        print(output)
+
+    except Exception as e:
+        print(f'[red]Erro ao executar comandos: {e}❌[/red]')
+        output = None
+
+    finally:
+        ssh.disconnect()
+        print(f'[yellow]Desconectado do {hostname}⚠️[/yellow]')
+
+    return output
+
+
+test_dmos(
+    hostname='100.127.0.145 ',
+    username='auto.noc',
+    password='3#ed$32wSEd5',
+)
