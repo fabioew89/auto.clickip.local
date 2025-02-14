b82defd5506668d7b9dfcbb9d7c94047ce1f2d52
Fabio Ewerton <fabioew89@gmail.com>
2025-02-14 12:23:23 -0400

    fix: add ensure_admin as function in create_admin

--- create_admin.py
+++ create_admin.py
@@ -10,22 +10,24 @@ fernet_key = Fernet(os.getenv('MY_FERNET_KEY'))
 
 app = create_app()
 
-with app.app_context():
-    admin_username = os.getenv('ADMIN_USERNAME')
-    admin_password = os.getenv('ADMIN_PASSWORD')
 
-    if not admin_username or not admin_password:
-        raise ValueError("ADMIN_USERNAME ou ADMIN_PASSWORD não encontrados no arquivo .env")  # noqa E501
+def ensure_admin():
+    with app.app_context():
+        admin_username = os.getenv('ADMIN_USERNAME')
+        admin_password = os.getenv('ADMIN_PASSWORD')
 
-    auto_noc = Users.query.filter_by(username=admin_username).first()
+        if not admin_username or not admin_password:
+            raise ValueError("ADMIN_USERNAME ou ADMIN_PASSWORD não encontrados no arquivo .env")  # noqa E501
 
-    if not auto_noc:
-        auto_noc = Users(
-            username=admin_username,
-            password=fernet_key.encrypt(admin_password.encode('utf-8')),
-        )
-        db.session.add(auto_noc)
-        db.session.commit()
-        print("🚀 Usuário admin criado com sucesso!")
-    else:
-        print("✅ Usuário admin já existe.")
+        auto_noc = Users.query.filter_by(username=admin_username).first()
+
+        if not auto_noc:
+            auto_noc = Users(
+                username=admin_username,
+                password=fernet_key.encrypt(admin_password.encode('utf-8')),
+            )
+            db.session.add(auto_noc)
+            db.session.commit()
+            print("🚀 Usuário admin criado com sucesso!")
+        else:
+            print("✅ Usuário admin já existe.")
--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 from app import create_app
 from app.controllers.admin import flask_admin
+from create_admin import ensure_admin
 
 app = create_app()
 
 flask_admin()
+ensure_admin()
 
 if __name__ == "__main__":
     app.run()
