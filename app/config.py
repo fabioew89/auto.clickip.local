0319e408043d3e0f6df8ed4b302aa5d391f2c301
Fabio Ewerton <fabioew89@gmail.com>
2025-03-10 16:51:54 -0400

    doc: added some comments in app.config.py to better readability

--- app/config.py
+++ app/config.py
@@ -1,8 +1,14 @@
 import os
 from dotenv import load_dotenv
+
+# Load environment variables from a .env file
 load_dotenv()
 
+# Define the database file name
 DATABASE_FILE = "app.db"
+
+# Set the SQLAlchemy database URI using SQLite
 SQLALCHEMY_DATABASE_URI = "sqlite:///" + DATABASE_FILE
 
+# Load the secret key from environment variables for security purposes
 SECRET_KEY = os.getenv('SQLALCHEMY_SECRET_KEY')
