b5c4a317a952bb0cdd16ed1b12565785cac72d78
Fabio Ewerton <fabioew89@gmail.com>
2025-03-14 18:06:52 -0400

    fix: updated livereloader

--- livereloader.py
+++ livereloader.py
@@ -7,6 +7,6 @@ app = create_app()
 def web_reloader():
     app.debug = True  # debug mode is required for templates to be reloaded
     server = Server(app.wsgi_app)
-    server.watch('app/templates')
-    server.watch('app/static')
+    server.watch('app/templates/**/*.*')
+    server.watch('app/static/**/*.*')
     server.serve(port=5000)
