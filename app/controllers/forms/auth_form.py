131b416b2a6a65ab7867f25898aac6f5861caf50
Fabio Ewerton <fabioew89@gmail.com>
2025-03-20 10:28:14 -0400

    fix: upt app/controllers/forms/auth_form.py

--- app/controllers/forms/auth_form.py
+++ app/controllers/forms/auth_form.py
@@ -3,6 +3,28 @@ from wtforms.validators import DataRequired, Length, EqualTo
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 
 
+class LoginForm(FlaskForm):
+    username = StringField(
+        'Username', validators=[
+            DataRequired(),
+            Length(max=30),
+        ]
+    )
+
+    password = PasswordField(
+        'Password', validators=[
+            DataRequired(),
+            Length(max=30),
+        ]
+    )
+
+    checkbox = BooleanField(
+        'Remember me!', default=True
+    )
+
+    submit = SubmitField('Entrar')
+
+
 class RegisterForm(FlaskForm):
     username = StringField(
         'Username', validators=[
@@ -31,28 +53,6 @@ class RegisterForm(FlaskForm):
             DataRequired()
         ],
         default=False
-    )
-
-    submit = SubmitField('Entrar')
-
-
-class LoginForm(FlaskForm):
-    username = StringField(
-        'Username', validators=[
-            DataRequired(),
-            Length(max=30),
-        ]
-    )
-
-    password = PasswordField(
-        'Password', validators=[
-            DataRequired(),
-            Length(max=30),
-        ]
-    )
-
-    checkbox = BooleanField(
-        'Remember me!', default=True
     )
 
     submit = SubmitField('Entrar')
