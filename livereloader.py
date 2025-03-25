2660c507780433d8c23dfe958b8e9e7c06a3d8f8
Fabio <fabioew89@gmail.com>
2025-03-25 09:59:23 -0400

    fix: upt livereloader

--- livereloader.py
+++ livereloader.py
@@ -7,27 +7,28 @@ app = create_app()
 
 def register_assets():
     assets = Environment(app)
-
     scss = Bundle(
         "scss/styles.scss",
         filters="libsass",
         output="css/styles.css"
     )
-
     assets.register("scss_all", scss)
     assets.auto_build = True
     assets.debug = True
-
-    scss.build()
+    return scss.build()
 
 
 def web_reloader():
     app.debug = True
-
     register_assets()
 
     server = Server(app.wsgi_app)
-    server.watch('app/templates/**/*.*')
-    server.watch('app/static/**/*.*')
+    server.watch('app/templates/**/*.*', delay=0.5)
+    server.watch('app/static/**/*.*', delay=0.5)
+    server.watch('app/static/scss/**/*.scss', lambda: register_assets())
 
-    server.serve(port=5000, restart_delay=2)
+    server.serve(port=app.config.get('PORT', 5000), restart_delay=1)
+
+
+if __name__ == '__main__':
+    web_reloader()
