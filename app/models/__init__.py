715c541f4203a56173132c3584059ab1de9a3b49
Fabio <fabioew89@gmail.com>
2025-04-01 17:28:58 -0400

    fix(app/models/__init__.py): add is_admin = db.Column(db.Boolean, default=False)

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -12,6 +12,7 @@ class Users(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String, nullable=False)
     password = db.Column(db.String, nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
 
     def __repr__(self):
         return self.username
