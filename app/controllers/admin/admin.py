3b7e973ec46b1f0d0e5c351c626662c9c3f02edd
Fabio <fabioew89@gmail.com>
2025-04-02 11:29:51 -0400

    feat(app/controllers/admin/admin.py): add new feature, fileadmin like uploads in admin painel

--- app/controllers/admin/admin.py
+++ app/controllers/admin/admin.py
@@ -1,6 +1,6 @@
 import os
+import os.path as op
 from app import db, admin
-from flask import url_for
 from dotenv import load_dotenv
 from flask import url_for, redirect
 from flask_login import current_user
@@ -8,10 +8,12 @@ from flask_admin.menu import MenuLink
 from cryptography.fernet import Fernet
 from flask_admin.contrib.sqla import ModelView
 from wtforms import StringField, PasswordField
+from flask_admin.contrib.fileadmin import FileAdmin
 from wtforms.validators import DataRequired, Length, EqualTo, IPAddress, InputRequired
 from app.models import Users, Routers, Switches, Olts, NeighborBgpIpv4, NeighborBgpIpv6
 
 load_dotenv()
+path = op.join(op.dirname(__file__), 'static')
 
 
 class UsersView(ModelView):
@@ -189,6 +191,19 @@ class LogoutLink(MenuLink):
         return url_for('auth_bp.logout')
 
 
+class UploadsFileAdmin(FileAdmin):
+    can_upload = True
+
+    can_delete = True
+    can_delete_dirs = True
+
+    can_mkdir = True
+    can_rename = True
+
+    allowed_extensions = None
+    editable_extensions = ()
+
+
 def flask_admin():
     admin.name = 'auto.clickip.local'
 
@@ -196,12 +211,8 @@ def flask_admin():
     admin.add_view(OltView(Olts, db.session, name='OLTs', category='Ativos'))
     admin.add_view(RoutersView(Routers, db.session, category='Ativos'))
     admin.add_view(SwitchView(Switches, db.session, category='Ativos'))
-    admin.add_view(OltView(Olts, db.session, category='Ativos'))
     admin.add_view(BgpNeighborIpv4View(NeighborBgpIpv4, db.session, category='Neighbor'))
     admin.add_view(BgpNeighborIpv6View(NeighborBgpIpv6, db.session, category='Neighbor'))
-
-    class LogoutLink(MenuLink):
-        def get_url(self):
-            return url_for('auth_bp.logout')
+    admin.add_view(UploadsFileAdmin(path, '/static/', name='Uploads'))
 
     admin.add_link(LogoutLink(name='Logout', category=''))
