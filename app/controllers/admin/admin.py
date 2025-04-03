5998212ac6a9f204aee3b87f5535933fa41f1fc2
Fabio <fabioew89@gmail.com>
2025-04-03 17:39:41 -0400

    feat(app/controllers/admin/admin.py): refact admin.py

--- app/controllers/admin/admin.py
+++ app/controllers/admin/admin.py
@@ -1,188 +1,114 @@
 import os
-import os.path as op
 from app import db, admin
 from dotenv import load_dotenv
+
 from flask import url_for, redirect
 from flask_login import current_user
 from flask_admin.menu import MenuLink
-from cryptography.fernet import Fernet
 from flask_admin.contrib.sqla import ModelView
-from wtforms import StringField, PasswordField
 from flask_admin.contrib.fileadmin import FileAdmin
-from wtforms.validators import DataRequired, Length, EqualTo, IPAddress, InputRequired
+
+from wtforms import PasswordField
+from wtforms.validators import Length, IPAddress, InputRequired
 from app.models import Users, Routers, Switches, Olts, NeighborBgpIpv4, NeighborBgpIpv6
+from cryptography.fernet import Fernet
 
 load_dotenv()
-path = op.join(op.dirname(__file__), 'static')
+path = os.path.join(os.path.dirname(__file__), 'static')
 
 
-class UsersView(ModelView):
+class UserView(ModelView):
     def is_accessible(self):
         return current_user.is_authenticated and current_user.is_admin
 
     def inaccessible_callback(self, name, **kwargs):
         return redirect(url_for('auth_bp.login'))
 
-    can_edit = True
-    can_delete = False
-    can_create = True
-    can_export = True
-
-    edit_modal = True
-    details_modal = True
-
-    can_view_details = True
-    can_set_page_size = True
+    edit_modal = details_modal = True
+    can_view_details = can_set_page_size = True
+    can_edit = can_create = can_export = can_delete = True
 
     column_default_sort = 'username'
     column_exclude_list = 'password'
 
-    form_extra_fields = {
-        'username': StringField(
-            validators=[
-                DataRequired(),
-                Length(min=5, max=30)
-            ]
-        ),
-        'password': PasswordField(
-            validators=[
-                DataRequired(),
-                Length(min=6)
-            ]
-        ),
-        'Password Confirm': PasswordField(
-            validators=[
-                DataRequired(),
-                EqualTo('password', message='Your password must be match')
-            ]
-        ),
+    column_searchable_list = column_filters = column_editable_list = ['username', 'is_admin']
+
+    form_overrides = {
+        'password': PasswordField
+    }
+
+    form_args = {
+        'password': {
+            'validators': [
+                InputRequired(),
+                Length(min=6, max=30)
+            ],
+        }
     }
 
     def on_model_change(self, form, model, is_created):
         fernet_key = Fernet(os.getenv('MY_FERNET_KEY'))
         if form.password.data:
-            model.password = fernet_key.encrypt(
-                form.password.data.encode('utf-8')
-            )
+            model.password = fernet_key.encrypt(form.password.data.encode('utf-8'))
 
 
-class RoutersView(ModelView):
-    can_edit = True
+class DeviceView(ModelView):
+    def is_accessible(self):
+        return current_user.is_authenticated
+
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(url_for('auth_bp.login'))
+
+    edit_modal = details_modal = True
+    can_view_details = can_set_page_size = True
+    can_edit = can_create = can_export = True
+
     can_delete = False
-    can_create = True
-    can_export = True
-
-    edit_modal = True
-    details_modal = True
-
-    can_view_details = True
-    can_set_page_size = True
 
     column_default_sort = 'hostname'
+    column_searchable_list = column_filters = column_editable_list = ['hostname', 'ip_address']
 
-    form_extra_fields = {
-        'ip_address': StringField(
-            'IP Address', validators=[
-                InputRequired(),
-                IPAddress(ipv4=True)
-            ]
-        ),
+    form_args = {
+        'ip_address': {
+            'label': 'Endereço IP',
+            'validators': [InputRequired(), IPAddress(ipv4=True)],
+            'description': 'Digite um IPv4 válido (ex: 192.168.1.1)'
+        }
+    }
+
+    form_widget_args = {
+        'ip_address': {
+            'placeholder': '192.168.1.1',
+        }
     }
 
 
-class SwitchView(ModelView):
-    can_edit = True
-    can_delete = False
-    can_create = True
-    can_export = True
-
-    edit_modal = True
-    details_modal = True
-
-    can_view_details = True
-    can_set_page_size = True
-
-    column_default_sort = 'hostname'
-
-    form_extra_fields = {
-        'ip_address': StringField(
-            'IP Address', validators=[
-                InputRequired(),
-                IPAddress(ipv4=True)
-            ]
-        ),
-    }
-
-
-class OltView(ModelView):
-    can_edit = True
-    can_delete = False
-    can_create = True
-    can_export = True
-
-    edit_modal = True
-    details_modal = True
-
-    can_view_details = True
-    can_set_page_size = True
-
-    column_default_sort = 'hostname'
-
-    form_extra_fields = {
-        'ip_address': StringField(
-            'IP Address', validators=[
-                InputRequired(),
-                IPAddress(ipv4=True)
-            ]
-        ),
-    }
-
-
-class BgpNeighborIpv4View(ModelView):
-    can_edit = True
-    can_delete = False
-    can_create = True
-    can_export = True
-
-    edit_modal = True
-    details_modal = True
-
-    can_view_details = True
-    can_set_page_size = True
-
+class BgpNeighborView(DeviceView):
     column_default_sort = 'description'
+    column_searchable_list = column_filters = column_editable_list = ['description', 'neighbor']
 
-    form_extra_fields = {
-        'neighbor': StringField(
-            'Neighbor', validators=[
-                InputRequired(),
-                IPAddress(ipv4=True)
-            ]
-        ),
+
+class BgpNeighborIpv4View(BgpNeighborView):
+    column_labels = dict(neighbor='Neighbor IPv4')
+
+    form_args = {
+        'neighbor': {
+            'label': 'Endereço IP',
+            'validators': [InputRequired(), IPAddress(ipv4=True)],
+            'description': 'Digite um IPv4 válido (ex: 192.168.1.1)'
+        }
     }
 
 
-class BgpNeighborIpv6View(ModelView):
-    can_edit = True
-    can_delete = False
-    can_create = True
-    can_export = True
+class BgpNeighborIpv6View(BgpNeighborView):
+    column_labels = dict(neighbor='Neighbor IPv6')
 
-    edit_modal = True
-    details_modal = True
-
-    can_view_details = True
-    can_set_page_size = True
-
-    column_default_sort = 'description'
-
-    form_extra_fields = {
-        'neighbor': StringField(
-            'Neighbor', validators=[
-                InputRequired(),
-                IPAddress(ipv6=True)
-            ]
-        ),
+    form_args = {
+        'neighbor': {
+            'label': 'Endereço IP',
+            'validators': [InputRequired(), IPAddress(ipv6=True, ipv4=False)],
+            'description': 'Digite um IPv6 válido (ex: 2001:db8::1)'
+        }
     }
 
 
@@ -192,13 +118,8 @@ class LogoutLink(MenuLink):
 
 
 class UploadsFileAdmin(FileAdmin):
-    can_upload = True
-
-    can_delete = True
-    can_delete_dirs = True
-
-    can_mkdir = True
-    can_rename = True
+    can_upload = can_delete = can_delete_dirs = True
+    can_mkdir = can_rename = True
 
     allowed_extensions = None
     editable_extensions = ()
@@ -207,12 +128,15 @@ class UploadsFileAdmin(FileAdmin):
 def flask_admin():
     admin.name = 'auto.clickip.local'
 
-    admin.add_view(UsersView(Users, db.session))
-    admin.add_view(OltView(Olts, db.session, name='OLTs', category='Ativos'))
-    admin.add_view(RoutersView(Routers, db.session, category='Ativos'))
-    admin.add_view(SwitchView(Switches, db.session, category='Ativos'))
+    admin.add_view(UserView(Users, db.session))
+
+    admin.add_view(DeviceView(Olts, db.session, name='OLTs', category='Ativos'))
+    admin.add_view(DeviceView(Routers, db.session, category='Ativos'))
+    admin.add_view(DeviceView(Switches, db.session, category='Ativos'))
+
     admin.add_view(BgpNeighborIpv4View(NeighborBgpIpv4, db.session, category='Neighbor'))
     admin.add_view(BgpNeighborIpv6View(NeighborBgpIpv6, db.session, category='Neighbor'))
+
     admin.add_view(UploadsFileAdmin(path, '/static/', name='Uploads'))
 
     admin.add_link(LogoutLink(name='Logout', category=''))
