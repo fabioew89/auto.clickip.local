2c3e41aa34f41642fde00889135aecaf6357e955
Fabio Ewerton <fabioew89@gmail.com>
2025-04-14 11:01:11 -0400

    fix(app/controllers/netmiko/dmos/mpls_seg_one.py): WIP in mpls_seg_one.py

--- app/controllers/netmiko/dmos/mpls_seg_one.py
+++ app/controllers/netmiko/dmos/mpls_seg_one.py
@@ -1,96 +1,97 @@
-from rich import print
-from netmiko import ConnectHandler
-"""
-Configura uma VLAN no dispositivo Datacom DMOS.
-"""
+# from rich import print
+# from netmiko import ConnectHandler
+# """
+# Configura uma VLAN no dispositivo Datacom DMOS.
+# """
 
-def mpls_seg_one(hostname, username, password, vlan, description, ipv4_neighbor):
-    switch = {
-        'device_type': 'cisco_ios',
-        'host': hostname,
-        'username': username,
-        'password': password,
-        'port': 22,  # Porta SSH padrão
-        'timeout': 30,  # Timeout de 30 segundos para a conexão
-        'session_timeout': 30,  # Tempo máximo para a sessão SSH
-    }
 
-    try:
-        ssh = ConnectHandler(**switch)
-        print(f'[green]Conectado ao {hostname}✅[/green]')
+# def mpls_seg_one(hostname, username, password, vlan, description, ipv4_neighbor):
+#     switch = {
+#         'device_type': 'cisco_ios',
+#         'host': hostname,
+#         'username': username,
+#         'password': password,
+#         'port': 22,  # Porta SSH padrão
+#         'timeout': 30,  # Timeout de 30 segundos para a conexão
+#         'session_timeout': 30,  # Tempo máximo para a sessão SSH
+#     }
 
-        commands = [
-            'config exclusive',
-            f'mpls l2vpn vpls-group tunnel-{vlan} vpn {description} description "{description}" administrative-status up',
-            f'mpls l2vpn vpls-group tunnel-{vlan} vpn {description} vfi pw-type vlan {vlan} neighbor {ipv4_neighbor} pw-id {vlan} pw-mtu 1600 pw-load-balance flow-label both',
-        ]
+#     try:
+#         ssh = ConnectHandler(**switch)
+#         print(f'[green]Conectado ao {hostname}✅[/green]')
 
-        for command in commands:
-            output = ssh.send_command(command, expect_string=r'#', read_timeout=15)
+#         commands = [
+#             'config exclusive',
+#             f'mpls l2vpn vpls-group tunnel-{vlan} vpn {description} description "{description}" administrative-status up',
+#             f'mpls l2vpn vpls-group tunnel-{vlan} vpn {description} vfi pw-type vlan {vlan} neighbor {ipv4_neighbor} pw-id {vlan} pw-mtu 1600 pw-load-balance flow-label both',
+#         ]
 
-            # 'config exclusive',
-            # f'mpls l2vpn vpls-group tunnel-{vlan}'           \
-            #     'vpn {description}'                           \
-            #         'description "{description}"' ;           \
-            #     administrative-status up',
-            # f'mpls l2vpn vpls-group tunnel-{vlan}           \
-            #     vpn {description} ;                         \
-            #         vfi                                     \
-            #             pw-type vlan {vlan} ;               \
-            #             neighbor {ipv4_neighbor} pw-id {vlan} ; \
-            #             pw-mtu 1600 ;                       \
-            #             pw-load-balance                     \
-            #                 flow-label both',
-            # f'mpls l2vpn vpls-group tunnel-{vlan}           \
-            #     vpn {description} ;                         \
-            #         bridge-domain                           \
-            #             dot1q {vlan} ;                      \
-            #             access-interface ten-gigabit-ethernet-1/1/1',
-            # 'commit confirmed 3; top',
-        # ]
+#         for command in commands:
+#             output = ssh.send_command(command, expect_string=r'#', read_timeout=15)
 
-        # output = ssh.send_command('show mpls l2vpn vpls brief', expect_string=r'#')
+#             # 'config exclusive',
+#             # f'mpls l2vpn vpls-group tunnel-{vlan}'           \
+#             #     'vpn {description}'                           \
+#             #         'description "{description}"' ;           \
+#             #     administrative-status up',
+#             # f'mpls l2vpn vpls-group tunnel-{vlan}           \
+#             #     vpn {description} ;                         \
+#             #         vfi                                     \
+#             #             pw-type vlan {vlan} ;               \
+#             #             neighbor {ipv4_neighbor} pw-id {vlan} ; \
+#             #             pw-mtu 1600 ;                       \
+#             #             pw-load-balance                     \
+#             #                 flow-label both',
+#             # f'mpls l2vpn vpls-group tunnel-{vlan}           \
+#             #     vpn {description} ;                         \
+#             #         bridge-domain                           \
+#             #             dot1q {vlan} ;                      \
+#             #             access-interface ten-gigabit-ethernet-1/1/1',
+#             # 'commit confirmed 3; top',
+#         # ]
 
-        # ssh.send_command('config exclusive', expect_string=r'#')
+#         # output = ssh.send_command('show mpls l2vpn vpls brief', expect_string=r'#')
 
-        # ssh.send_command(  # descricao do VPLS
-        #     f'mpls l2vpn vpls-group tunnel-{vlan}           \
-        #         vpn {description}                           \
-        #             description "{description}" ;           \
-        #         administrative-status up', expect_string=r'#'d
-        #     )
+#         # ssh.send_command('config exclusive', expect_string=r'#')
 
-        # ssh.send_command(
-        #     f'mpls l2vpn vpls-group tunnel-{vlan}           \
-        #     vpn {description} ;                             \
-        #         vfi                                         \
-        #             pw-type vlan {vlan} ;                   \
-        #             neighbor {ipv4_neighbor} pw-id {vlan} ; \
-        #             pw-mtu 1600 ;                           \
-        #             pw-load-balance                         \
-        #                 flow-label both', expect_string=r'#'
-        #     )
+#         # ssh.send_command(  # descricao do VPLS
+#         #     f'mpls l2vpn vpls-group tunnel-{vlan}           \
+#         #         vpn {description}                           \
+#         #             description "{description}" ;           \
+#         #         administrative-status up', expect_string=r'#'d
+#         #     )
 
-        # ssh.send_command(
-        #     f'mpls l2vpn vpls-group tunnel-{vlan}           \
-        #     vpn {description} ;                             \
-        #         bridge-domain                               \
-        #             dot1q {vlan} ;                          \
-        #             access-interface ten-gigabit-ethernet-1/1/1', expect_string=r'#'
-        #     )
+#         # ssh.send_command(
+#         #     f'mpls l2vpn vpls-group tunnel-{vlan}           \
+#         #     vpn {description} ;                             \
+#         #         vfi                                         \
+#         #             pw-type vlan {vlan} ;                   \
+#         #             neighbor {ipv4_neighbor} pw-id {vlan} ; \
+#         #             pw-mtu 1600 ;                           \
+#         #             pw-load-balance                         \
+#         #                 flow-label both', expect_string=r'#'
+#         #     )
 
-        # ssh.send_command('commit confirmed 3; top', expect_string=r'#')
+#         # ssh.send_command(
+#         #     f'mpls l2vpn vpls-group tunnel-{vlan}           \
+#         #     vpn {description} ;                             \
+#         #         bridge-domain                               \
+#         #             dot1q {vlan} ;                          \
+#         #             access-interface ten-gigabit-ethernet-1/1/1', expect_string=r'#'
+#         #     )
 
-        # output = ssh.send_command(f'show mpls l2vpn vpls-group brief | include {vlan}', expect_string=r'#')
+#         # ssh.send_command('commit confirmed 3; top', expect_string=r'#')
 
-        # print(output)
+#         # output = ssh.send_command(f'show mpls l2vpn vpls-group brief | include {vlan}', expect_string=r'#')
 
-    except Exception as e:
-        print(f'Error to execute commands:{e}❌')
-        # output = None
+#         # print(output)
 
-    finally:
-        ssh.disconnect()
-        print(f'[yellow]Desconectado do {hostname}⚠️[/yellow]')
+#     except Exception as e:
+#         print(f'Error to execute commands:{e}❌')
+#         # output = None
 
-    # return output
+#     finally:
+#         ssh.disconnect()
+#         print(f'[yellow]Desconectado do {hostname}⚠️[/yellow]')
+
+#     # return output
