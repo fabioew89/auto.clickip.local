636fbb41ac7ae8607c0386412c2d047c61bb6002
Arnon Alvim <alvimarnon@gmail.com>
2025-06-13 09:58:03 -0400

    fix: clear code

--- app/controllers/forms/downstream_fec.py
+++ app/controllers/forms/downstream_fec.py
@@ -16,16 +16,8 @@ class Downstream_fec_form(FlaskForm):
     )
 
     port_id = SelectField(
-        'gpon', choices=[
-            ('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'),
-            ('5', '5'), ('6', '6'), ('7', '7'), ('8', '8'),
-            ('9', '9'), ('10', '10'), ('11', '11'), ('12', '12'),
-            ('13', '13'), ('14', '14'), ('15', '15'), ('16', '16'),
-            ('17', '17'), ('18', '18'), ('19', '19'), ('20', '20'),
-            ('21', '21'), ('22', '22'), ('23', '23'), ('24', '24'),
-            ('25', '25'), ('26', '26'), ('27', '27'), ('28', '28'),
-            ('29', '29'), ('30', '30'), ('31', '31'), ('32', '32')
-        ]
+        'gpon',
+        choices=[(str(i), str(i)) for i in range(1, 33)]
     )
 
     dmos_command = SelectField(
@@ -34,7 +26,6 @@ class Downstream_fec_form(FlaskForm):
             ('downstream-fec', 'Habilitar'),
             ('no downstream-fec', 'Desabilitar'),
         ],
-        # default='downstream-fec'
     )
 
     submit = SubmitField('Commit')
